96d47e191d7829845fbca481809061569f3830de
talent-jsdev <webdevelopercrane@gmail.com>
2022-10-21 16:11:57 -0500

    avoid multi times in case of no task

--- index.js
+++ index.js
@@ -8,20 +8,21 @@ app.post('/api/runTasks', async (req, res, next) => {
     const taskIds = req.body.taskIds;
     let resOrder = [];
 
-    try {
-        taskIds.forEach(async taskId => {
-            if (!TaskRunner.hasTask(taskId)) {
-                res.status(404).json({ "error": "no Task" });
-            }
+    for (let i = 0; i < taskIds.length; i++) {
+        if (!TaskRunner.hasTask(taskIds[i])) {
+            res.status(404).json({ "error": "no Task" });
+        }
+    }
 
+    try {
+        taskIds.forEach(async taskId => {              
             await TaskRunner.runTask(taskId)
             resOrder.push(taskIds.indexOf(taskId));
 
             if (resOrder.length == taskIds.length) {
                 res.status(200).json(getResult(resOrder));
             }
-        })
-
+        })    
     } catch (error) {
         next(error);
     }
